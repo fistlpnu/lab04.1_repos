8e05382538983c4716ef613d3978f4745981bee2
Olexandr <fistovsky.laptop@gmail.com>
2024-10-14 14:31:55 +0300

    first commit

---
+++ LabAP_4.1/Lab_04_1.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int N, k;
+	double S;
+
+	cout << "N = "; cin >> N;
+	cout << "k = "; cin >> k;
+
+	S = 0;
+	k = 1;
+	while (k <= N)
+	{
+		S += 1 + sqrt(1 + k * k) / pow(k, 2);
+		k++;
+	}
+	cout << S << endl;
+	
+	S = 0;
+	k = 1;
+	do {
+		S += 1 + sqrt(1 + k * k) / pow(k, 2);
+		k++;
+	} while (k <= N);
+	cout << S << endl;
+	
+	S = 0;
+	for (k = 1; k <= N; k++)
+	{
+		S += 1 + sqrt(1 + k * k) / pow(k, 2);
+	}
+	cout << S << endl;
+
+	S = 0;
+	for (k = N; k >= 1; k--)
+	{
+		S += 1 + sqrt(1 + k * k) / pow(k, 2);
+	}
+	cout << S << endl;
+
+	return 0;
+}
